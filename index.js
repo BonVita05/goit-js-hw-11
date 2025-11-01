5b432672550cc027821ec6f96474e73ccd5ccf99
BonVita05 <vitabvita@gmail.com>
2025-11-01 16:34:17 +0000

    Deploying to gh-pages from @ BonVita05/goit-js-hw-11@524e0d4a2e04b200641c8304bcaa628eeca099a4 🚀

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+/* empty css                      */import{a as p,S as d,i as m}from"./assets/vendor-woitT8w3.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();function g(r){return p("https://pixabay.com/api/",{params:{key:"53047751-cea4bcad23f1cdb0ee3074407",q:`${r}`,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:12}}).then(({data:n})=>n)}const h=new d(".gallery a",{caption:!0,captionDelay:250,captionPosition:"bottom",nav:!0,showCounter:!0,loop:!0}),a=document.querySelector(".gallery"),c=document.querySelector(".loading"),u=document.querySelector(".loader"),f=document.querySelector("#searchBtn");function y(r=[]){a.innerHTML=r.map(({previewURL:i,tags:o,largeImageURL:n,likes:e,views:t,comments:s,downloads:l})=>`
+        <li class="gallery-item">
+            <a class="gallery-link" href="${n}">
+                <img  class="gallery-image" src="${i}" alt="${o}" loading="lazy" 
+                title="Title: ${o.split(",")[0].trim()}  |  
+                Likes: ${e.toLocaleString()}  |  
+                View: ${t.toLocaleString()}  |  
+                Comments: ${s.toLocaleString()}  |  
+                Downloads: ${l.toLocaleString()}"/>
+            </a>
+            <ul class="info-container">
+                <li class="info-box">
+                    <p class="info-title">Likes</p>
+                    <p class="info-value">${e.toLocaleString()}</p>
+                </li>
+                <li class="info-box">
+                    <p class="info-title">View</p>
+                    <p class="info-value">${t.toLocaleString()}</p>
+                </li>
+                 <li class="info-box">
+                    <p class="info-title">Comments</p>
+                    <p class="info-value">${s.toLocaleString()}</p>
+                </li>
+                <li class="info-box">
+                    <p class="info-title">Downloads</p>
+                    <p class="info-value">${l.toLocaleString()}</p>
+                </li>
+            </ul>
+        </li>
+    `).join(""),h.refresh()}function L(){a.innerHTML=""}function S(){c.hidden=!1}function b(){c.hidden=!0}function $(){u.hidden=!1,f.hidden=!1}function w(){u.hidden=!0,f.hidden=!0}const x=document.querySelector(".form");x.addEventListener("submit",r=>{r.preventDefault(),L();const i=r.target.elements["serch-text"].value.trim();S(),$(),g(i).then(o=>{if(!o.hits.length)throw new Error("Sorry, there are no images matching your search query. Please try again!");y(o.hits)}).catch(o=>{m.error({message:`${o.message??String(err)}`,position:"topCenter",timeout:3e3,backgroundColor:"#EF4040",messageColor:"white",close:!1})}).finally(()=>{r.target.elements["serch-text"].value="",b(),w()})});
 //# sourceMappingURL=index.js.map
